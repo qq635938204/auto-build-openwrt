7ed766e8180e6df2f7857dd8f78b96e80379f0aa
Zhang3woshi <yingxong-4402@126.com>
2022-04-19 08:59:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,5 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#1. Modify default IP
+sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
